f3ba2a361a664fe5f948a645697da17bbcdc118a
cmoch <65452402+cmoch@users.noreply.github.com>
2020-06-11 10:28:11 -0400

    Refresh Date Variable Added

--- robo_advisor.py
+++ robo_advisor.py
@@ -7,16 +7,17 @@ import requests
 
 
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 
 response = requests.get(request_url)
 #print(type(response))
 #print(response.status_code)
 #print(response.text)
-
 parsed_response = json.loads(response.text)
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"] 
 
-breakpoint()
+
+#breakpoint()
 
 
 
@@ -28,7 +29,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
